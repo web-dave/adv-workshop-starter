76d9160e9a13a29a5769193a1d0e5cbde7b3cf84
web-dave <dave@webdave.de>
2022-03-24 12:31:38 +0100

    maintain Observables

--- src/app/swapi/crawler/crawler.component.ts
+++ src/app/swapi/crawler/crawler.component.ts
@@ -2,10 +2,23 @@ import {
   Component,
   Input,
   OnChanges,
+  OnDestroy,
   OnInit,
   SimpleChanges,
 } from '@angular/core';
-import { NEVER, Observable, pluck, tap } from 'rxjs';
+import {
+  first,
+  interval,
+  NEVER,
+  Observable,
+  pluck,
+  Subject,
+  Subscriber,
+  Subscription,
+  take,
+  takeUntil,
+  tap,
+} from 'rxjs';
 import { IFilm } from '../models/film.interface';
 
 @Component({
@@ -13,10 +26,35 @@ import { IFilm } from '../models/film.interface';
   templateUrl: './crawler.component.html',
   styles: [],
 })
-export class CrawlerComponent implements OnChanges {
-  @Input() film: string | undefined = '';
+export class CrawlerComponent implements OnChanges, OnInit, OnDestroy {
+  @Input() film: string | null = '';
+  @Input() crawl$: Observable<number> = NEVER;
+  subs: Subscription[] = [];
+  sub = new Subscription();
+  kill$ = new Subject<number>();
   constructor() {}
+  ngOnInit(): void {
+    this.sub.add(interval(4000).subscribe());
+    interval(4000)
+      .pipe(take(1))
+      .subscribe((data) => console.log('take1', data));
+    interval(4000)
+      .pipe(first())
+      .subscribe((data) => console.log('first', data));
+    interval(4000)
+      .pipe(takeUntil(this.kill$))
+      .subscribe((data) => console.log('kill', data));
+    this.crawl$.subscribe((data) => console.log('!!', data));
+  }
+
   ngOnChanges(changes: SimpleChanges): void {
     console.log(changes);
+  }
+  ngOnDestroy(): void {
+    this.kill$.next(0);
+    this.kill$.complete();
+    this.subs.forEach((s) => s.unsubscribe());
+    this.sub.unsubscribe();
+    console.error('TOT');
   }
 }
--- src/app/swapi/films/film/film.component.ts
+++ src/app/swapi/films/film/film.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { NEVER, Observable, share, shareReplay } from 'rxjs';
+import {
+  filter,
+  interval,
+  NEVER,
+  Observable,
+  pluck,
+  share,
+  shareReplay,
+} from 'rxjs';
 import { ApiService } from '../../api.service';
 import { IFilm } from '../../models/film.interface';
 
@@ -17,15 +25,21 @@ import { IFilm } from '../../models/film.interface';
 })
 export class FilmComponent implements OnInit, OnDestroy {
   film$: Observable<IFilm> = NEVER;
+  crawl$: Observable<string> = NEVER;
+  foo$ = interval(1500);
+  show = false;
   constructor(private route: ActivatedRoute, private api: ApiService) {}
   ngOnDestroy(): void {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(
-      (data) =>
-        (this.film$ = this.api
-          .getEntity<IFilm>('films', data['id'])
-          .pipe(shareReplay()))
-    );
+    this.route.params.subscribe((data) => {
+      this.film$ = this.api
+        .getEntity<IFilm>('films', data['id'])
+        .pipe(shareReplay());
+      this.crawl$ = this.film$.pipe(
+        filter((film) => film.director === 'George Lucas'),
+        pluck('opening_crawl')
+      );
+    });
   }
 }
