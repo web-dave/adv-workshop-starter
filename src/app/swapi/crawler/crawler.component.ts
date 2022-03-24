30b9a391a48b33532137c199598b284fea786023
web-dave <dave@webdave.de>
2022-03-24 12:33:23 +0100

    make it nice

--- src/app/swapi/crawler/crawler.component.ts
+++ src/app/swapi/crawler/crawler.component.ts
@@ -29,31 +29,16 @@ import { IFilm } from '../models/film.interface';
 export class CrawlerComponent implements OnChanges, OnInit, OnDestroy {
   @Input() film: string | null = '';
   @Input() crawl$: Observable<number> = NEVER;
-  subs: Subscription[] = [];
   sub = new Subscription();
-  kill$ = new Subject<number>();
   constructor() {}
   ngOnInit(): void {
     this.sub.add(interval(4000).subscribe());
-    interval(4000)
-      .pipe(take(1))
-      .subscribe((data) => console.log('take1', data));
-    interval(4000)
-      .pipe(first())
-      .subscribe((data) => console.log('first', data));
-    interval(4000)
-      .pipe(takeUntil(this.kill$))
-      .subscribe((data) => console.log('kill', data));
-    this.crawl$.subscribe((data) => console.log('!!', data));
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     console.log(changes);
   }
   ngOnDestroy(): void {
-    this.kill$.next(0);
-    this.kill$.complete();
-    this.subs.forEach((s) => s.unsubscribe());
     this.sub.unsubscribe();
     console.error('TOT');
   }
